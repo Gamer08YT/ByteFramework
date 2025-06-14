8d33c14592832f56c8fa44a50d3e5d5fd1a44f87
Jan Heil <jan.heil@byte-store.de>
2025-06-14 17:32:43 +0200

    Add `Label` component to index page setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 #include <Arduino.h>
 #include "service/PageService.h"
 #include "core/components/Button.h"
+#include "core/components/Label.h"
 
 void setup()
 {
     auto index = PageService::addPage("/", new Page());
 
     index->setTitle("ESP32 Index");
+    index->addComponent(new Label("Test 123"));
     index->addComponent(new Button("submit", "Click me!"));
 }
 
