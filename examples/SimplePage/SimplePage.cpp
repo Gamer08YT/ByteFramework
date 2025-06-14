b01f080d4e4515942b55119329ab11f1c6b95fc1
Jan Heil <jan.heil@byte-store.de>
2025-06-14 17:47:02 +0200

    Setup WiFi Access Point and integrate `AsyncWebServer` in `SimplePage` example
    Configured an ESP32 WiFi Access Point with "ByteFramework" SSID and initialized `AsyncWebServer`. Updated the `SimplePage` example to demonstrate a functional page with components using `PageService`. Added a delay in the loop to reduce CPU usage.

---
+++ examples/SimplePage/SimplePage.cpp
@@ -0,0 +1,41 @@
+//
+// Created by JanHe on 14.06.2025.
+//
+#include <Arduino.h>
+#include <WiFi.h>
+
+#include "../../src/service/PageService.h"
+#include "../../src/core/components/Button.h"
+#include "../../src/core/components/Label.h"
+
+AsyncWebServer server(80);
+
+void setup()
+{
+    // Begin Serial.
+    Serial.begin(115200);
+
+    // Setup WiFi AP.
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("ByteFramework");
+
+    // Begin Page Service.
+    PageService::begin(&server);
+
+
+    // Build simple Page.
+    auto index = PageService::addPage("/", new Page());
+
+    index->setTitle("ESP32 Index");
+    index->addComponent(new Label("Test 123"));
+    index->addComponent(new Button("submit", "Click me!"));
+
+    // Begin Server.
+    server.begin();
+}
+
+// Simple lock to save CPU Bandwith.
+void loop()
+{
+    delay(100);
+}
